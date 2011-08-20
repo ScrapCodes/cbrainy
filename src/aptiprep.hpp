d26fa0784dc878b6c0b3d3921064b2d2bab2724f
prashant <prashant@prashant.(none)>
2011-08-20 22:03:03 +0530

    Some corrections

--- src/aptiprep.hpp
+++ src/aptiprep.hpp
@@ -5,9 +5,8 @@
 * This file is distributed under GPLv2 http://www.gnu.org/licenses/gpl-2.0.html
 * Copyright (C) 2011  Prashant Sharma
 * This program is free software; you can redistribute it and/or
-* modify it under the terms of the GNU General Public License
-* as published by the Free Software Foundation; either version 2
-* of the License, or (at your option) any later version.
+* modify it under the terms of the GNU General Public License version 2
+* as published by the Free Software Foundation.
 
 * This program is distributed in the hope that it will be useful,
 * but WITHOUT ANY WARRANTY; without even the implied warranty of
